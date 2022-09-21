f1ad1b0bbb75b5ca6e81e4ef19c8b1eabf1daa1b
Александр <decomp30000012@mail.ru>
2022-09-21 23:42:56 +0300

    projectRM

---
+++ projectRM/projectRM/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  projectRM
+//
+//  Created by Александр Троицкий on 21.09.2022.
+//
+
+import UIKit
+/// FirstVC
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions) {
+    }
+}

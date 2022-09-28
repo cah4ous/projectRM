1a667b3faf6708a30209735c8c63f0cf32753376
Александр <decomp30000012@mail.ru>
2022-09-28 07:24:23 +0300

    Добавил функцию расшаривания чека

--- projectRM/projectRM/Views/CheckViewController.swift
+++ projectRM/projectRM/Views/CheckViewController.swift
@@ -9,21 +9,46 @@ import UIKit
 /// Экран, который отвечает за выдачу чека о покупке пользователю
 class CheckViewController: UIViewController {
 
-    var createInfoLabel = UILabel()
+    var infoLabel = UILabel()
     var infoText = "nil"
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        initMethods()
+    }
+    
+    func initMethods() {
         createPriceLabel()
+        createButtonShare()
     }
     
     func createPriceLabel() {
-        createInfoLabel.text = infoText
-        createInfoLabel.font = .boldSystemFont(ofSize: 20)
-        createInfoLabel.numberOfLines = 0
-        createInfoLabel.textAlignment = .center
-        createInfoLabel.frame = CGRect(x: 50, y: 100, width: 320, height: 100)
+        infoLabel.text = infoText
+        infoLabel.font = .boldSystemFont(ofSize: 20)
+        infoLabel.numberOfLines = 0
+        infoLabel.textAlignment = .center
+        infoLabel.frame = CGRect(x: 50, y: 100, width: 320, height: 100)
         
-        view.addSubview(createInfoLabel)
+        view.addSubview(infoLabel)
+    }
+    
+    func createButtonShare() {
+        let buttonShare = UIButton()
+
+        buttonShare.layer.cornerRadius = 5
+        buttonShare.backgroundColor = .blue
+        buttonShare.setTitle("Расшарить", for: .normal)
+        buttonShare.addTarget(self, action: #selector(shareAction), for: .touchUpInside)
+        
+        buttonShare.frame = CGRect(x: 50, y: 480, width: 300, height: 75)
+
+        view.addSubview(buttonShare)
+
+    }
+
+    @objc func shareAction() {
+        let items = [infoLabel.text ?? "nil"]
+        let activityViewController = UIActivityViewController(activityItems: items, applicationActivities: nil)
+        present(activityViewController, animated: true)
     }
 }

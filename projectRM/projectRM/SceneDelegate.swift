17aa01b3cba3f16451cb5d83e980923768d8aca1
Александр <decomp30000012@mail.ru>
2022-10-03 21:43:20 +0300

    Сделал 12-ое дз

--- projectRM/projectRM/SceneDelegate.swift
+++ projectRM/projectRM/SceneDelegate.swift
@@ -15,5 +15,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         _ scene: UIScene,
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions) {
+            if let windowScene = scene as? UIWindowScene {
+                let window = UIWindow(windowScene: windowScene)
+                let navigationViewController = UINavigationController(rootViewController: TextRedactorViewController())
+                
+                let tabBarController = UITabBarController()
+                tabBarController.setViewControllers([navigationViewController], animated: true)
+                                                    
+                window.rootViewController = tabBarController
+                self.window = window
+                window.makeKeyAndVisible()
+              }
     }
 }
